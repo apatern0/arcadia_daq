dc7bd0fb9d8be1c23e52c5cc5b83e7aa5139d033
Giuseppe Gebbia <giuseppegebbia92@gmail.com>
2021-09-23 11:17:09 +0200

    fix bindings with pointers

--- DAQ_pybind.cpp
+++ DAQ_pybind.cpp
@@ -23,21 +23,49 @@ void set_ipbus_loglevel(int level){
 }
 
 
+//TODO: find something cleaner for functions taking pointer arguments
 PYBIND11_MODULE(DAQ_pybind, m) {
 
 	py::class_<DAQBoard_comm>(m, "DAQBoard_comm")
 		.def(py::init<const std::string &, const std::string &, bool>())
 		.def("read_conf", &DAQBoard_comm::read_conf)
-		.def("spi_transfer", &DAQBoard_comm::spi_transfer)
-		.def("read_gcr", &DAQBoard_comm::read_gcr)
+
+		.def("spi_transfer", [](DAQBoard_comm &DAQ, ARCADIA_command command, uint16_t payload,
+					std::string chip_id) {
+				uint32_t rcv_data;
+				int ret = DAQ.spi_transfer(command, payload, chip_id, &rcv_data);
+				return std::make_tuple(ret, rcv_data);
+				})
+
+		.def("read_gcr", [](DAQBoard_comm &DAQ, std::string chip_id, uint16_t addr,
+					bool force_update) {
+				uint16_t value;
+				int ret = DAQ.read_gcr(chip_id, addr, &value, force_update);
+				return std::make_tuple(ret, value);
+				})
+
 		.def("write_gcr", &DAQBoard_comm::write_gcr)
 		.def("reinitialize_gcr", &DAQBoard_comm::reinitialize_gcr)
 		.def("write_icr", &DAQBoard_comm::write_icr)
 		.def("write_gcrpar", &DAQBoard_comm::write_gcrpar)
-		.def("read_gcrpar", &DAQBoard_comm::read_gcrpar)
+
+		.def("read_gcrpar", [](DAQBoard_comm &DAQ, std::string chip_id, std::string gcrpar,
+					bool force_update) {
+				uint16_t value;
+				int ret = DAQ.read_gcrpar(chip_id, gcrpar, &value, force_update);
+				return std::make_tuple(ret, value);
+				})
+
 		.def("check_consistency", &DAQBoard_comm::check_consistency)
 		.def("send_controller_command", &DAQBoard_comm::send_controller_command)
 		.def("read_fpga_register", &DAQBoard_comm::read_fpga_register)
+
+		.def("read_fpga_register", [](DAQBoard_comm &DAQ, std::string reg_handler) {
+				uint32_t value;
+				int ret = DAQ.read_fpga_register(reg_handler, &value);
+				return std::make_tuple(ret, value);
+				})
+
 		.def("write_fpga_register", &DAQBoard_comm::write_fpga_register)
 		.def("send_pulse", &DAQBoard_comm::send_pulse)
 		.def("dump_DAQBoard_reg", &DAQBoard_comm::dump_DAQBoard_reg)
