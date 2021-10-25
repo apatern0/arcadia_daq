22453d044a68ac4fd21d0c2335c84bac0a8d58ce
Andrea Paterno <andrea.paterno@to.infn.it>
2021-10-25 18:32:59 +0200

    Thorough comments and lint on daq.py

--- pyarcadia/daq.py
+++ pyarcadia/daq.py
@@ -55,9 +55,10 @@ def onecold(bits, base=0xffff):
     return ~onehot(bits) & base
 
 class Fpga(FPGAIf):
-    """This class is used to communicate with the FPGA
+    """This class is used to communicate with the FPGA.
+    It extends the C++ Class implementation FPGAIf, by providing
+    a more python-friendly interface.
 
-    :ivar chip_id: Chip identification. Default: id0
     :ivar lanes_masked: Sections to filter out during FPGA readout
     """
 
@@ -74,19 +75,28 @@ class Fpga(FPGAIf):
             super().__init__(xml_file, 'kc705', 0)
         except:
             raise RuntimeError('Failed to instantiate FPGAIf')
-            sys.exit(255)
 
     def __init__(self, xml_file=None):
         self.init_connection(xml_file)
 
     def get_chip(self, chip_id):
+        """Retrieve a Chip instance for the specified chip id.
+
+        :param chip_id: Number of the chip to retrieve [0,1,2]
+        :type chip_id: int
+        """
         return Chip(chip_id, self.chips[chip_id])
 
-class Chip(object):
-    def __init__(self, id, chipif):
+class Chip:
+    """The Chip class is used to interface with a chip. It
+    references the ChipIf interface ported from C++, and wraps
+    its methods in order to provide auxiliary features.
+    """
+
+    def __init__(self, chip_id, chipif):
         self.__chipif = chipif
 
-        self.id = id
+        self.id = chip_id
         self.lanes_masked = []
         self.pcr = None
         self.track_pcr = False
@@ -170,28 +180,26 @@ class Chip(object):
         pixsel   = self.read_gcrpar('HELPER_SECCFG_PIXELSELECT')
         cfgval   = self.read_gcrpar('HELPER_SECCFG_CFGDATA')
 
-        for pr in range(prstart, prstop+1, prskip+1):
-            pix_row_base = pr*4 + ((pixsel >> 4) & 0b1)*2
-            for pix in range(0,4):
+        for region in range(prstart, prstop+1, prskip+1):
+            pix_row_base = region*4 + ((pixsel >> 4) & 0b1)*2
+            for pix in range(0, 4):
                 if ((pixsel >> pix) & 0b1) == 0:
                     continue
 
                 pix_row = pix_row_base + math.floor(pix/2)
                 pix_col_base = (pix % 2)
 
-                for sec in range(0,16):
+                for sec in range(0, 16):
                     if (secs >> sec) & 0b1 == 0:
                         continue
 
-                    for col in range(0,16):
+                    for col in range(0, 16):
                         if (cols >> col) & 0b1 == 0:
                             continue
 
                         pix_col = pix_col_base + sec*32 + col*2
 
                         self.pcr[pix_row][pix_col] = cfgval
-
-
 
     def send_controller_command(self, cmd, value=0):
         """Send a command to the FPGA Controller
@@ -211,44 +219,47 @@ class Chip(object):
         return self.__chipif.packets_reset()
 
     def packets_count(self):
-        """Get the current FPGA FIFO occupancy
+        """Get the number of available data packets
 
-        :return: Data packets in the FPGA FIFO
+        :return: Number of available packets
         :rtype: int
         """
         return self.__chipif.packets_count()
 
     def packets_read(self, packets=0):
-        """Get `packets` readout from DAQ FIFO
+        """Read data packets from the chip
 
-        :return: Number of packets readout
-        :rtype: int
+        :param packets: Number of packets to retrieve (0 for max)
+        :type packets: int, optional
+
+        :return: Packets list
+        :rtype: list of uint64
         """
         return self.__chipif.packets_read(packets)
 
     def packets_read_stop(self):
-        """Waits for the data readout to stop
+        """Stops the automatic readout of packets from the FPGA FIFO
         """
         return self.__chipif.packets_read_stop()
 
-    def readout(self):
-        """Fetch readout packets
+    def enable_readout(self, lanes):
+        """Enable data readout from the specified lanes
 
-        :return: Data packets readout so far
-        :rtype: py::array_t
+        :param lanes: Lanes to enable
+        :type lanes: int or list of ints
         """
-        return self.__chipif.readout()
-
-    def enable_readout(self, sections):
-        """Enable t
-
-        :return: Data packets in the FPGA FIFO
-        :rtype: int
-        """
-        sections = onehot(sections) & onecold(self.lanes_masked)
-        self.send_controller_command('setTxDataEnable', sections)
+        lanes = onehot(lanes) & onecold(self.lanes_masked)
+        self.send_controller_command('setTxDataEnable', lanes)
 
     def custom_word(self, word, payload=0):
+        """Inserts a custom word in the FPGA's FIFO.
+
+        :param word: Word to insert
+        :type word: int
+
+        :param payload: Optional 8-bit payload to the word
+        :type payload: int
+        """
         self.send_controller_command('loadUserData_0', ((word<<8) | (payload & 0xff)) & 0xffff)
         self.send_controller_command('loadUserData_1', (word>>8 & 0xffff))
         self.send_controller_command('loadUserData_2', (word>>24 & 0xffff))
@@ -256,10 +267,23 @@ class Chip(object):
         self.send_controller_command('loadUserDataPush', 0)
 
     def set_timestamp_delta(self, delta):
+        """Sets the FPGA timestamp delta. Used for timestamp synchronization
+        between FPGA and chip.
+
+        :param delta: Delta to set
+        :type delta: int
+        """
         self.send_controller_command('loadTSDeltaLSB', ((delta>>0)  & 0xfffff))
         self.send_controller_command('loadTSDeltaMSB', ((delta>>20) & 0xfffff))
 
     def calibrate_deserializers(self):
+        """Trigger the calibration of the deserializers. The procedure tries
+        to select and set the optimal Tap Delays in order to minimize sampling
+        errors.
+
+        :return: Lanes that have been successfully calibrated
+        :rtype: list of ints
+        """
         self.sync_mode()
         time.sleep(0.01)
         response = self.__chipif.calibrate_deserializers()
@@ -274,27 +298,50 @@ class Chip(object):
         return lanes
 
     def set_timestamp_period(self, period):
+        """Set the timestamp period in the FPGA timestamp counter.
+
+        :param period: Period in FPGA Clock cycles
+        :type period: int
+        """
         period = period
         self.send_controller_command('writeTimeStampPeriod', (period & 0xffff))
 
     # Chip commands
     def hard_reset(self):
+        """Sends a hard reset to the chip through the Reset pin.
+        """
         self.send_controller_command('doRESET', 0x1)
 
     def soft_reset(self):
+        """Sends a hard reset to the chip through ICR resets.
+        """
         self.write_icr(0, 0x0015)
 
     def space_mode(self):
+        """Configures the chip to operate in space mode. Only lane 0 will be activated.
+        """
         self.write_gcrpar('OPERATION', 1)
 
     def normal_mode(self):
+        """Configures the chip to operate in normal mode. All the lanes will be activated.
+        """
         self.write_gcrpar('SERIALIZER_SYNC', 0)
         self.write_gcrpar('OPERATION', 0)
 
     def sync_mode(self):
+        """Configures the chip to operate in sync mode. Lanes only send synchronization words.
+        """
         self.write_gcrpar('SERIALIZER_SYNC', 1)
 
     def reset_subsystem(self, subsystem, action=0):
+        """Uses the built-in reset mechanism to reset on-chip subsystems.
+
+        :param subsystem: The subsystem to reset [chip, gcr, per, sec, ts]
+        :type subsystem: string
+
+        :param action: Reset operation [0: pulse, 1: start, 2: stop]
+        :type action: int
+        """
         sub_dict = {
             'chip': 3,
             'gcr':  4,
@@ -312,78 +359,225 @@ class Chip(object):
         self.write_icr(0, icr0)
 
     # Onehot/Onecold GCRs
-    def gcr_onehot(self, gcr, sections=0xffff, update=False):
+    def gcr_onehot(self, gcr, value=0xffff, update=False):
+        """Configures a GCR to a one-hot encoded value
+
+        :param gcr: GCR to configure
+        :type gcr: int
+
+        :param value: Value to be used
+        :type action: int or list of ints
+
+        :param update: Update previous value with new bits enabled or substitute
+        :type update: bool
+        """
+
         base = self.read_gcr(gcr) if update else 0x0000
-        sections = onehot(sections, base)
-    
-        self.write_gcr(gcr, sections)
+        value = onehot(value, base)
 
-    def gcr_onecold(self, gcr, sections=0xffff, update=False):
+        self.write_gcr(gcr, value)
+
+    def gcr_onecold(self, gcr, value=0xffff, update=False):
+        """Configures a GCR to a one-cold encoded value
+
+        :param gcr: GCR to configure
+        :type gcr: int
+
+        :param value: Value to be used
+        :type action: int or list of ints
+
+        :param update: Update previous value with new bits enabled or substitute
+        :type update: bool
+        """
+
         base = self.read_gcr(gcr) if update else 0xffff
-        sections = onecold(sections, base)
+        value = onecold(value, base)
 
-        self.write_gcr(gcr, sections)
+        self.write_gcr(gcr, value)
 
     def injection_disable(self, sections=0xffff, update=False):
+        """Disables injection on selected sections.
+
+        :param sections: Sections to disable
+        :type sections: int or list of ints
+
+        :param update: Update from previous value or substitute
+        :type update: bool
+        """
         self.gcr_onehot(2, sections, update)
 
     def injection_enable(self, sections=0xffff, update=False):
+        """Enables injection on selected sections.
+
+        :param sections: Sections to enable
+        :type sections: int or list of ints
+
+        :param update: Update from previous value or substitute
+        :type update: bool
+        """
         self.gcr_onecold(2, sections, update)
 
     def read_disable(self, sections=0xffff, update=False):
+        """Disables readout on selected sections.
+
+        :param sections: Sections to disable
+        :type sections: int or list of ints
+
+        :param update: Update from previous value or substitute
+        :type update: bool
+        """
         self.gcr_onehot(2, sections, update)
 
     def read_enable(self, sections=0xffff, update=False):
+        """Enables readout on selected sections.
+
+        :param sections: Sections to enable
+        :type sections: int or list of ints
+
+        :param update: Update from previous value or substitute
+        :type update: bool
+        """
         self.gcr_onecold(2, sections, update)
 
     def clock_disable(self, sections=0xffff, update=False):
+        """Disables clock on selected sections.
+
+        :param sections: Sections to disable
+        :type sections: int or list of ints
+
+        :param update: Update from previous value or substitute
+        :type update: bool
+        """
         self.gcr_onehot(3, sections, update)
 
     def clock_enable(self, sections=0xffff, update=False):
+        """Enables clock on selected sections.
+
+        :param sections: Sections to enable
+        :type sections: int or list of ints
+
+        :param update: Update from previous value or substitute
+        :type update: bool
+        """
         self.gcr_onecold(3, sections, update)
 
     def injection_digital(self, sections=0xffff, update=False):
+        """Enables digital injection on selected sections.
+
+        :param sections: Sections
+        :type sections: int or list of ints
+
+        :param update: Update from previous value or substitute
+        :type update: bool
+        """
         self.gcr_onehot(4, sections, update)
 
     def injection_analog(self, sections=0xffff, update=False):
+        """Enables analog injection on selected sections.
+
+        :param sections: Sections
+        :type sections: int or list of ints
+
+        :param update: Update from previous value or substitute
+        :type update: bool
+        """
         self.gcr_onecold(4, sections, update)
 
     def force_injection(self, sections=0xffff, update=False):
+        """Override pixel's PCR to force injection on selected sections.
+
+        :param sections: Sections
+        :type sections: int or list of ints
+
+        :param update: Update from previous value or substitute
+        :type update: bool
+        """
         self.gcr_onehot(5, sections, update)
 
     def noforce_injection(self, sections=0xffff, update=False):
+        """Disables pixel's PCR override to force injection on selected sections.
+
+        :param sections: Sections
+        :type sections: int or list of ints
+
+        :param update: Update from previous value or substitute
+        :type update: bool
+        """
         self.gcr_onecold(5, sections, update)
 
     def force_nomask(self, sections=0xffff, update=False):
         self.gcr_onehot(6, sections, update)
+        """Override pixel's PCR to disable mask on selected sections.
+
+        :param sections: Sections
+        :type sections: int or list of ints
+
+        :param update: Update from previous value or substitute
+        :type update: bool
+        """
 
     def noforce_nomask(self, sections=0xffff, update=False):
+        """Disables pixel's PCR override to disable mask on selected sections.
+
+        :param sections: Sections
+        :type sections: int or list of ints
+
+        :param update: Update from previous value or substitute
+        :type update: bool
+        """
         self.gcr_onecold(6, sections, update)
 
     # PCRs
     def write_pcr(self):
+        """Start the pixel's PCR programming procedure
+        """
         self.write_icr(0, onehot([8]))
 
     def pixel_cfg(self, cfg, p):
+        """Configure a pixel with a specific PCR value.
+
+        :param cfg: Value to configure the PCR to
+        :type cfg: 2-bit int
+
+        :param p: Pixel to configure
+        :type p: Pixel instance
+        """
         self.pixels_cfg(cfg, [p.get_sec()], [p.get_dcol()], [p.get_corepr()], [p.get_master()], [p.get_idx()])
 
     def pixels_cfg(self, cfg, sections = 0xffff, columns = 0xffff, prs=None, master=None, pixels = 0xf):
+        """Configure a set of pixels with a specific PCR value.
+
+        :param sections: Sections
+        :type sections: int or list of ints
+
+        :param columns: Columns
+        :type columns: int or list of ints
+
+        :param prs: Pixel Regions
+        :type prs: list of ints
+
+        :param master: Master sub-PR or Slave
+        :type master: int or list of ints
+
+        :param pixels: Pixels in the sub-PR
+        :type pixels: int or list of ints
+        """
         self.write_gcrpar('HELPER_SECCFG_SECTIONS', onehot(sections))
         self.write_gcrpar('HELPER_SECCFG_COLUMNS',  onehot(columns))
 
-        if(prs == None):
+        if prs is None:
             prs = [[0, 127]]
-        elif(isinstance(prs, int)):
+        elif isinstance(prs, int):
             prs = [[prs]]
 
-        if(master == None):
+        if master is None:
             master = [0, 1]
 
         for pr_range in prs:
-            if(isinstance(pr_range, int)):
+            if isinstance(pr_range, int):
                 range_start = pr_range
                 range_stop  = pr_range
-            elif(isinstance(pr_range, list) and len(pr_range) == 1):
+            elif isinstance(pr_range, list) and len(pr_range) == 1:
                 range_start = pr_range[0]
                 range_stop  = pr_range[0]
             else:
@@ -397,17 +591,45 @@ class Chip(object):
 
             master = [master] if not isinstance(master, list) else master
             for subpr in master:
-                #self.logger.info("Configuring [%x][%x][%d]->[%d] %s[%s] w/ %x" % (onehot(sections), onehot(columns), range_start, range_stop, str(master), format(onehot(pixels), '#06b')[2:], cfg))
                 pselect = (subpr << 4) | (onehot(pixels) & 0xf)
                 self.write_gcrpar('HELPER_SECCFG_PIXELSELECT', pselect)
                 self.write_pcr()
                 time.sleep(0.001)
 
-    def pixels_mask(self, sections = 0xffff, columns = 0xffff, prs=None, master=None, pixels = 0xf):
+    def pixels_mask(self, sections=0xffff, columns=0xffff, prs=None, master=None, pixels = 0xf):
+        """Mask a set of pixels
+
+        :param sections: Sections
+        :type sections: int or list of ints
+
+        :param columns: Columns
+        :type columns: int or list of ints
+
+        :param prs: Pixel Regions
+        :type prs: list of ints
+
+        :param master: Master sub-PR or Slave
+        :type master: int or list of ints
+
+        :param pixels: Pixels in the sub-PR
+        :type pixels: int or list of ints
+        """
         self.pixels_cfg(0b11, sections, columns, prs, master, pixels)
 
     # Injection
     def send_tp(self, pulses=1, t_on=1000, t_off=1000):
+        """Send a Test Pulse train to the chip.
+
+        :param pulses: Number of Test Pulses to send
+        :type pulses: int
+
+        :param t_on: TP high state duration in number of clock cycles
+        :type t_on: int
+
+        :param t_off: TP low state duration in number of clock cycles
+        :type t_off: int
+        """
+
         if t_on > (1<<20):
             raise ValueError("TP On Time (%d) exceeds maximum value of %d." % (t_on, (1<<20)))
 
@@ -422,12 +644,20 @@ class Chip(object):
 
     # FPGA commands
     def sync(self, lanes=0xffff):
+        """Trigger the FPGA synchronization procedure on specific lanes.
+
+        :param lanes: Lanes to synchronize
+        :type lanes: int or list of ints
+
+        :return: List of synchronized lanes
+        :rtype: list of ints
+        """
         self.send_controller_command('syncTX', onehot(lanes))
         ret, response = self.send_controller_command('readTxState', 0)
 
         lanes = []
         for i in range(16):
-            if ((response >> i) & 0b1):
+            if (response >> i) & 0b1:
                 lanes.append(i)
 
         return lanes
